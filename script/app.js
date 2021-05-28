636b05371aa7998fd22311a4129ba7c4a1b23556
Your Name <ineta.bliu@gmail.com>
2021-05-28 18:03:02 +0100

    Changes made to array

--- script/app.js
+++ script/app.js
@@ -3,9 +3,14 @@ function init() {
   const grid = document.querySelector('.grid')
   const width = 10
   const height = 20 
-  const blocks = []
-
+  const state = []
   
+  // const state = new Array(width * height)
+  // let elements
+  let blockCurrentPosition = 4
+  
+
+
   // Grid with x & y coordinates as id's on it.
   function createPlayfield(){
     for (let y = height - 1; y >= 0; y--) {
@@ -13,19 +18,65 @@ function init() {
         const block = document.createElement('div')
         block.setAttribute('id', `${x}-${y}`)
         grid.appendChild(block)
-        blocks.push(block)
-        block.innerText = [`y ${y} - x ${x}`]
+        state.push(block)
+        block.innerText = [`x ${x} - y ${y}`]
       }  
     }
-    console.log('my blocks->', blocks)
-  }
-  // Coloring one block on the screen
-  function renderBlock(y, x, color){
-    const myBlock = document.getElementById(`${x}-${y}`)
-    myBlock.style.backgroundColor = color
+    console.log('my blocks->', state)
   }
 
+  const blockTimer = setInterval(moveBlock, 1000)
+
+  function moveBlock() {
+    const blockNewPosition = blockCurrentPosition + width
+    if (blockNewPosition > state.length) {
+      clearInterval(blockTimer)
+      return 
+    }
+    removeBlock(blockCurrentPosition)
+    blockCurrentPosition = blockNewPosition
+    addBlock(blockCurrentPosition)
+  }
+  function addBlock(position) {
+    state[position].classList.add('block')
+  }
+
+  function removeBlock(position) {
+    state[position].classList.remove('block')
+  }
+
+  
+
+
+
+
+
+
+
   createPlayfield()
-  renderBlock(19, 4, 'red')
+
+
+
+
+  
+
 }
+
+
 window.addEventListener('DOMContentLoaded', init)
+
+
+
+
+
+/// trash and used code 
+
+
+
+// coloring the block using ID
+// Coloring one block on the screen
+// function renderBlock(y, x, color){
+//   const myBlock = document.getElementById(`${x}-${y}`)
+//   myBlock.style.backgroundColor = color
+// }
+// renderBlock(19, 4, 'red')
