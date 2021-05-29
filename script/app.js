ee5f2bfff6af85d69eac05ed9ae9cb0623f3fbd2
Your Name <ineta.bliu@gmail.com>
2021-05-29 10:13:48 +0100

    Refactored move keys

--- script/app.js
+++ script/app.js
@@ -25,17 +25,18 @@ function init() {
     console.log('my blocks->', state)
   }
 
-  const blockTimer = setInterval(moveBlock, 1000)
+  const blockTimer = setInterval(moveBlock, 500)
 
   function moveBlock() {
     const blockNewPosition = blockCurrentPosition + width
     if (blockNewPosition > state.length) {
       clearInterval(blockTimer)
-      return 
+      return blockNewPosition
     }
     removeBlock(blockCurrentPosition)
     blockCurrentPosition = blockNewPosition
     addBlock(blockCurrentPosition)
+    addBlock(blockNewPosition)
   }
   function addBlock(position) {
     state[position].classList.add('block')
@@ -45,21 +46,40 @@ function init() {
     state[position].classList.remove('block')
   }
 
-  
 
+  //move elements to the right
+  function moveRight(event) {
+    console.log(event.keyCode)
+    const keyRight = event.keyCode
+    removeBlock(blockCurrentPosition)
+    if (keyRight === 39 && blockCurrentPosition % width !==  width - 1) {
+      blockCurrentPosition++
+    } 
+  }
+  //move elements to the left
+  function moveLeft(event) {
+    const keyLeft = event.keyCode
+    removeBlock(blockCurrentPosition)
+    if (keyLeft === 37 && blockCurrentPosition % width !== 0) {
+      blockCurrentPosition--
+    } //else if ('logic for making the block speed up on the down')
+  }
 
-
-
-
-
+  function hardDrop(event) {
+    console.log(event.keyCode)
+    const keyDown = event.keyCode
+    if (keyDown === 40 && blockCurrentPosition + width <= width * width - 1) {
+      console.log('down')
+      blockCurrentPosition += width
+    }
+  }
+  console.log('my state length->', state.length)
+  document.addEventListener('keydown', moveRight)
+  document.addEventListener('keydown', moveLeft)
+  document.addEventListener('keydown', hardDrop)
 
   createPlayfield()
-
-
-
-
-  
-
+  console.log('my state length->', state.length)
 }
 
 
